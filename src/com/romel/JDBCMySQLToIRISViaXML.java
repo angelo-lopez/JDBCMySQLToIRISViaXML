9a072e9c62e04f04ddaf642789f78931e75d36d0
Angelo Romel <angeloromel@gmail.com>
2020-04-27 22:32:41 +0100

    Added Intersystems JDBC external jar file to class path. Added import packages for intersystems jdbc, java sql packages, dom and xml parser.

--- src/com/romel/JDBCMySQLToIRISViaXML.java
+++ src/com/romel/JDBCMySQLToIRISViaXML.java
@@ -1,5 +1,18 @@
 package com.romel;
 
+import com.intersystems.jdbc.*;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.File;
+
 public class JDBCMySQLToIRISViaXML {
 
 	public static void main(String[] args) {
